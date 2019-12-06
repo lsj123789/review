a86f9c74ea5f4ad8326a88da96296ab8d539ff74
今天刘思佳脸又圆了吗? <1375287603@qq.com>
2019-12-06 16:20:38 +0800

    把AABB式字符串转化为BBAA

--- regexp/AABB.js
+++ regexp/AABB.js
@@ -1,6 +1,6 @@
 // 将AABB式字符串 改成BBAA式
 function changeAABBToBBAA(str) {
     let reg = /(\w)\1(\w)\2/g;
-    console.log(str.replace(reg, '$2$2$1$1'))
+    return str.replace(reg, '$2$2$1$1')
 }
 changeAABBToBBAA('aabb')

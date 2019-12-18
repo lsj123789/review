470acfd97018bc4365a050896f25f974e2eb6165
今天刘思佳脸又圆了吗? <1375287603@qq.com>
2019-12-18 11:04:27 +0800

    使用immutable.js管理store中的对象

--- jianshu/src/common/header/store/reducer.js
+++ jianshu/src/common/header/store/reducer.js
@@ -2,21 +2,21 @@ import {
     SEARCH_FOCUS,
     SEARCH_BLUR
 } from './constants';
+import {
+    fromJS
+} from 'immutable';//把普通js对象变成一个immutable对象
 
-const defaultState = {
+const defaultState = fromJS({
     focused: false,
-};
+});
 
 export default (state = defaultState, action) => {
     if (action.type === SEARCH_FOCUS) {
-        return {
-            focused: true
-        }
+        //immutable对象的set方法，会结合之前immutable对象的值和设置的值，返回一个新对象
+        return state.set('focused', true)
     }
     if (action.type === SEARCH_BLUR) {
-        return {
-            focused: false
-        }
+        return state.set('focused', false)
     }
     return state
 }

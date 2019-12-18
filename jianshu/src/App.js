fc1418d4791583635a7444114b9bd68713cbc00e
今天刘思佳脸又圆了吗? <1375287603@qq.com>
2019-12-18 10:05:15 +0800

    使用react-redux进行应用数据管理

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { GlobalStyled } from './style'
 import { GlobalIcon } from './statics/iconfont/iconfont'
 import Header from './common/header'
+import store from './store'
 function App() {
   return (
-    <div>
+    <Provider store={store}>
       <GlobalStyled />
       <GlobalIcon />
       <Header />
-    </div>
+    </Provider>
   );
 }
 
--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,54 +1,60 @@
-import React ,{ Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import { WrapperHeader ,Logo, Nav , NavItem, NavSearch , Addition , Button , SearchWrapper} from './style'
 
-class Header extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            focused:false,
-        }
-    }
-    handleInputBlur = () => {
-        this.setState({
-            focused:false,
-        })
-    }
-    handleInputFocus = () => {
-        this.setState({
-            focused:true
-        })
-    }
-    render(){
-        const { focused } = this.state;
-        return(
-        <WrapperHeader>
-            <Logo />
-            <Nav>
-                <NavItem className='left active'>首页</NavItem>
-                <NavItem className='left'>下载APP</NavItem>
-                <NavItem className='right'>
-                    <i className='iconfont'>&#xe636;</i>
-                </NavItem>
-                <NavItem className='right'>登录</NavItem>
-                <SearchWrapper>
-                  <NavSearch 
-                    className={focused ? 'focused' : null} 
-                    onFocus={this.handleInputFocus}
-                    onBlur={this.handleInputBlur}
-                  />
-                  <i className={focused ? 'focused iconfont' : 'iconfont'}>&#xe601;</i>
-                </SearchWrapper>
-                <Addition>
-                    <Button className='reg'>注册</Button>
-                    <Button className='writing'>
-                    <i className='iconfont'>&#xe617;</i>
-                        写文章
-                    </Button>
-                </Addition>
-            </Nav>
-        </WrapperHeader>
-        )
+const Header = props => {
+   const { focused,handleInputFocus,handleInputBlur } = props;
+    return(
+    <WrapperHeader>
+        <Logo />
+        <Nav>
+            <NavItem className='left active'>首页</NavItem>
+            <NavItem className='left'>下载APP</NavItem>
+            <NavItem className='right'>
+                <i className='iconfont'>&#xe636;</i>
+            </NavItem>
+            <NavItem className='right'>登录</NavItem>
+            <SearchWrapper>
+              <NavSearch 
+                className={focused ? 'focused' : null} 
+                onFocus={ handleInputFocus }
+                onBlur={ handleInputBlur }
+              />
+              <i className={focused ? 'focused iconfont' : 'iconfont'}>&#xe601;</i>
+            </SearchWrapper>
+            <Addition>
+                <Button className='reg'>注册</Button>
+                <Button className='writing'>
+                <i className='iconfont'>&#xe617;</i>
+                    写文章
+                </Button>
+            </Addition>
+        </Nav>
+    </WrapperHeader>
+    )
+}
+
+const mapStateToProps = state =>{
+    return{
+        focused:state.focused
     }
 }
 
-export default Header;
+const mapDispatchToProps = dispatch => {
+    return {
+        handleInputFocus(){
+            const action = {
+                type:'search_focus',
+            }
+            dispatch(action)
+        },
+        handleInputBlur(){
+            const action = {
+                type:'search_blur'
+            }
+            dispatch(action)
+        }
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Header);

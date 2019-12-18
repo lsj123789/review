5987e1653dcf7342b3c1d71f613f9795fa75fcd9
今天刘思佳脸又圆了吗? <1375287603@qq.com>
2019-12-18 15:12:38 +0800

    代码优化微调

--- jianshu/src/common/header/store/reducer.js
+++ jianshu/src/common/header/store/reducer.js
@@ -5,23 +5,22 @@ import {
 } from './constants';
 import {
     fromJS
-} from 'immutable';//把普通js对象变成一个immutable对象
+} from 'immutable'; //把普通js对象变成一个immutable对象
 
 const defaultState = fromJS({
     focused: false,
-    hotSearchList:[],
+    hotSearchList: [],
 });
 
 export default (state = defaultState, action) => {
-    if (action.type === SEARCH_FOCUS) {
-        //immutable对象的set方法，会结合之前immutable对象的值和设置的值，返回一个新对象
-        return state.set('focused', true)
+    switch (action.type) {
+        case SEARCH_FOCUS:
+            return state.set('focused', true)
+        case SEARCH_BLUR:
+            return state.set('focused', false)
+        case CHANGE_HOT_SEARCH_LIST:
+            return state.set('hotSearchList', action.data)
+        default:
+            return state
     }
-    if (action.type === SEARCH_BLUR) {
-        return state.set('focused', false)
-    }
-    if(action.type === CHANGE_HOT_SEARCH_LIST){
-        return state.set('hotSearchList' , action.data)
-    }
-    return state
 }

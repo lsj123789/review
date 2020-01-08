c85b444a04ede3670ac151b7ae027ab726a2ff56
今天刘思佳脸又圆了吗? <1375287603@qq.com>
2020-01-08 10:41:08 +0800

    冒泡排序

--- leetcode/sort/bubbleSort.js
+++ leetcode/sort/bubbleSort.js
@@ -1,8 +1,8 @@
 //冒泡排序
 
 let bubbleSort = arr => {
-    for (let i = arr.length - 1, tmp; i > 0; i++) {
-        for (let j = 0; j < i; j++) {
+    for (let i = arr.length - 1, tmp; i > 0; i++) { //外层循环控制循环边界 每次循环边界向左移动一位
+        for (let j = 0; j < i; j++) { //内层循环控制两两相比较 大的右移
             tmp = arr[j]
             if (tmp > arr[j + 1]) {
                 arr[j] = arr[j + 1]
